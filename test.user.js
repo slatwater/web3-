71d1fbc4f6aa9371e426ee675acc331a67a0ffad
teslaceo <82397505+slatwater@users.noreply.github.com>
2024-11-12 13:26:22 +0800

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动化脚本：Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse
 // @namespace    http://tampermonkey.net/
-// @version      3.7
+// @version      3.8
 // @description  自动化操作 Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse 页面上的任务
 // @author
 // @match        https://space3.gg/missions?search=&sort=NEWEST&page=1
@@ -1309,7 +1309,7 @@
         const url1 = 'https://pentagon.games/account';
         const url2 = 'https://pentagon.games/sign-in';
         const url3 = 'https://pentagon.games/airdrop/ascended';
-        const url4 = 'https://www.holoworldai.com/chat/YbkygYZ9lsDhCz5VbiRd';
+        const url4 = 'https://app.holoworld.com/chat/YbkygYZ9lsDhCz5VbiRd';
     
         // 通过XPath获取元素
         function getElementByXpath(xpath) {
