9522b48cc7b0094b3acac08b1236087d73cf4d85
teslaceo <82397505+slatwater@users.noreply.github.com>
2024-11-17 13:21:44 +0800

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动化脚本：Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse
 // @namespace    http://tampermonkey.net/
-// @version      4.7
+// @version      4.8
 // @description  自动化操作 Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse 页面上的任务
 // @author
 // @match        https://space3.gg/missions?search=&sort=NEWEST&page=1
@@ -1507,7 +1507,7 @@
         async function stepTwo() {
             log('第二步：等待元素1并点击');
     
-            var element1Xpath = '//*[@id="__next"]/div/div/div[1]/div[8]/div/div[1]/div[3]/div[2]/div[1]/button/img';
+            var element1Xpath = '//*[@id="__next"]/div/div/div[1]/div[7]/div/div[1]/div[3]/div[2]/div[1]/button/img';
             var maxChecks = 60; // 最大检查次数（相当于30秒）
             var checks = 0;
             var checkInterval = 500; // 每500ms检查一次
@@ -1534,7 +1534,7 @@
         async function waitForElement2() {
             log('第二步：等待元素2并点击');
     
-            var element2Xpath = '//*[@id="__next"]/div/div/div[1]/div[8]/div/div[6]/div[2]/div/div[2]/div[2]';
+            var element2Xpath = '//*[@id="__next"]/div/div/div[1]/div[7]/div/div[6]/div[2]/div/div[2]/div[2]';
             var maxChecks = 60;
             var checks = 0;
             var checkInterval = 500;
@@ -1565,7 +1565,7 @@
         async function waitForElement4() {
             log('第三步：等待元素4');
     
-            var element4Xpath = '//*[@id="__next"]/div/div/div[1]/div[8]/div/div[6]/div[2]/div/div[3]/div/div[2]/div/div/div[4]/button';
+            var element4Xpath = '//*[@id="__next"]/div/div/div[1]/div[7]/div/div[6]/div[2]/div/div[3]/div/div[2]/div/div/div[4]/button';
             var maxChecks = 60;
             var checks = 0;
             var checkInterval = 500;
