c2fa83f67a11834bafa3279b4cae1098f5fb6b74
teslaceo <82397505+slatwater@users.noreply.github.com>
2024-12-06 10:37:30 +0800

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动化脚本：Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse
 // @namespace    http://tampermonkey.net/
-// @version      6.7
+// @version      6.8
 // @description  自动化操作 Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse 页面上的任务
 // @author
 // @match        https://space3.gg/missions?search=&sort=NEWEST&page=1
@@ -1411,7 +1411,7 @@
                     await randomDelay(500, 1500);
     
                     log('等待元素2出现');
-                    const element2Xpath = '/html/body/div[2]/div/div/div/div[2]/div/div/div/div[2]/div/div/canvas';
+                    const element2Xpath = '/html/body/div[2]/div/div/div/div[2]/div/div/div/div/div/div/canvas';
                     const element2 = await waitForXPath(element2Xpath, 20000);
     
                     if (element2) {
