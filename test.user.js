e1aa6804bbfc082e50232dabb3b0f670f4f132e4
teslaceo <82397505+slatwater@users.noreply.github.com>
2025-01-31 11:46:13 +0800

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动化脚本：Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse
 // @namespace    http://tampermonkey.net/
-// @version      9.3
+// @version      9.5
 // @description  自动化操作 Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse 页面上的任务
 // @author
 // @match        https://space3.gg/missions?search=&sort=NEWEST&page=1
@@ -1402,7 +1402,7 @@
                         await new Promise(r => setTimeout(r, waitMs));
 
                         // Full Xpath for the 3-click element
-                        const tripleClickXPath = "/html/body/div[1]/main/div[3]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div[2]/button";
+                        const tripleClickXPath = "/html/body/div[1]/main/div[3]/div/div[3]/div[2]/div/div[2]/div[2]/div[2]/button";
                         const tripleClickElem = getElementByXPathScript2(tripleClickXPath);
                         if (tripleClickElem) {
                             for (let i=0; i<3; i++){
