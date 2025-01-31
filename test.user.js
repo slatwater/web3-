63fc7c7f79ebf030ce991f4d0ad832b061e1cf53
teslaceo <82397505+slatwater@users.noreply.github.com>
2025-01-31 11:43:16 +0800

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动化脚本：Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse
 // @namespace    http://tampermonkey.net/
-// @version      9.0
+// @version      9.3
 // @description  自动化操作 Space3、SideQuest、Glob Shaga Quests、Forge.gg、Reddio Points Task 和 XtremeVerse 页面上的任务
 // @author
 // @match        https://space3.gg/missions?search=&sort=NEWEST&page=1
@@ -1365,7 +1365,7 @@
 
             while (continueClicking) {
                 // 元素4的XPath
-                const elem4XPath = "/html/body/div[1]/main/div[3]/div/div[3]/div[2]/div[2]/div[1]/div[2]/div[2]/button";
+                const elem4XPath = "/html/body/div[1]/main/div[3]/div/div[3]/div[2]/div/div[1]/div[2]/div[2]/button";
                 const elem4Button = getElementByXPathScript2(elem4XPath);
                 if (elem4Button) {
                     logScript2('点击元素4');
