183c3ea60538dc9131bd427df0e88870214da0d0
teslaceo <82397505+slatwater@users.noreply.github.com>
2025-02-10 23:43:02 +0800

    Update sahara.user.js

--- sahara.user.js
+++ sahara.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sahara Labs 自动化脚本 (去调试日志 + 等待页面 + 并行监测切换/提交, 文本判定Approve/Disapprove)
 // @namespace    http://tampermonkey.net/
-// @version      11.7
+// @version      11.8
 // @description  1) 无调试日志 2) 等待页面加载完成+额外等待 3) 并行监测切换元素/提交元素可点击, 点击后再次操作一
 //               操作一：根据rawText是否属于预设文本 => Disapprove / Otherwise => Approve
 // @match        https://app.saharalabs.ai/*
@@ -122,6 +122,8 @@
         "A kitchen sink",
         "A paper shredder",
         "A desktop computer",
+        "A beach ball",
+        "A fishing rod",
         "A full-length mirror",
         "A flower vase",
         "A wind turbine",
@@ -238,6 +240,7 @@
         "A decorative vase",
         "A kitchen",
         "A Hospital",
+        "A mailbox",
         "A car battery",
         "A washing detergent",
         "A bicycle",
