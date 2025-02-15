a84950bc6e636f3da13e3620e4ced118fe32b4b8
teslaceo <82397505+slatwater@users.noreply.github.com>
2025-02-15 17:57:25 +0800

    Update sahara.user.js

--- sahara.user.js
+++ sahara.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sahara Labs 宽松政策
 // @namespace    http://tampermonkey.net/
-// @version      12.5
+// @version      12.6
 // @description  1) 无调试日志 2) 等待页面加载完成+额外等待 3) 并行监测切换元素/提交元素可点击, 点击后再次操作一
 //               操作一：根据rawText是否属于预设文本 => Disapprove / Otherwise => Approve
 // @match        https://app.saharalabs.ai/*
@@ -53,6 +53,7 @@
         "Jumpingjack",
         "Mountains, Valleys, Hills",
         "Moonwalk, Flyme, Skyfall",
+        "I like pizza",
         "Dogs, Cats, Hamsters",
         "Waterfalls and Oceans",
         "Cats, Dogs, Birds",
