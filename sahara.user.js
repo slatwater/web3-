075934b03cb1f37ddb22ba6e48fba450f0acfc1e
teslaceo <82397505+slatwater@users.noreply.github.com>
2025-02-27 15:40:58 +0800

    Update sahara.user.js

--- sahara.user.js
+++ sahara.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sahara Labs 宽松政策
 // @namespace    http://tampermonkey.net/
-// @version      15.9
+// @version      16.0
 // @description  1) 无调试日志 2) 等待页面加载完成+额外等待 3) 并行监测切换元素/提交元素可点击, 点击后再次操作一
 //               操作一：根据rawText是否属于预设文本 => Disapprove / Otherwise => Approve
 // @match        https://app.saharalabs.ai/*
@@ -21,6 +21,8 @@
         "cloudy rainbow",
         "Axolotyl",
         "Axolotl",
+        "flyinghousesplace",
+        "flymehome",
         "moonlight basket",
         "a friendly kitten",
         "talking dog",
