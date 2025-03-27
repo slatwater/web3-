75c163f09e23a97e56767454c69436e1885f800b
teslaceo <82397505+slatwater@users.noreply.github.com>
2025-03-27 15:30:28 +0800

    Update sahara2222.user.js

--- sahara2222.user.js
+++ sahara2222.user.js
@@ -5,8 +5,8 @@
 // @description  1) 无调试日志 2) 等待页面加载完成+额外等待 3) 并行监测切换元素/提交元素可点击, 点击后再次操作一
 //               操作一：根据rawText是否属于预设文本 => Disapprove / Otherwise => Approve
 // @match        https://app.saharalabs.ai/*
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/slatwater/web3-/raw/refs/heads/main/sahara2222.user.js
+// @downloadURL  https://github.com/slatwater/web3-/raw/refs/heads/main/sahara2222.user.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
